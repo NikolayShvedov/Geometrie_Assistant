0a46f1d0b4253a4a7024dfce3724d5231c1ef150
NikolayShvedov <52162935+NikolayShvedov@users.noreply.github.com>
2019-07-01 12:06:51 +0300

    Добавьте файлы проекта.

---
+++ myForm/AssemblyInfo.cpp
@@ -0,0 +1,22 @@
+#include "stdafx.h"
+
+using namespace System;
+using namespace System::Reflection;
+using namespace System::Runtime::CompilerServices;
+using namespace System::Runtime::InteropServices;
+using namespace System::Security::Permissions;
+
+[assembly:AssemblyTitleAttribute(L"myForm")];
+[assembly:AssemblyDescriptionAttribute(L"")];
+[assembly:AssemblyConfigurationAttribute(L"")];
+[assembly:AssemblyCompanyAttribute(L"HP Inc.")];
+[assembly:AssemblyProductAttribute(L"myForm")];
+[assembly:AssemblyCopyrightAttribute(L"(c) HP Inc. 2019")];
+[assembly:AssemblyTrademarkAttribute(L"")];
+[assembly:AssemblyCultureAttribute(L"")];
+
+[assembly:AssemblyVersionAttribute("1.0.*")];
+
+[assembly:ComVisible(false)];
+
+[assembly:CLSCompliantAttribute(true)];
